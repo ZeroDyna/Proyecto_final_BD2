461f39cdd91d7e1d7daa72b494d0f23ff234ac9e
Renato Corrales <renato98990@gmail.com>
2024-11-08 14:12:10 -0500

    intente hacer algo

--- Proyecto_final_BD2/Sector.cpp
+++ Proyecto_final_BD2/Sector.cpp
@@ -1,1 +1,11 @@
 #include "Sector.h"
+
+Sector::Sector(size_t size, std::vector<Tablas> tabla_) {
+	std::cin >> size;
+	tabla = tabla_;
+}
+bool Sector::insertar_datos(std::vector<char> test) {
+	//acá quiero hacer que el lenguaje de cconsulta haga un insert
+	//se acabo el chatgpt
+	
+}
--- Proyecto_final_BD2/Sector.h
+++ Proyecto_final_BD2/Sector.h
@@ -1,11 +1,16 @@
 #pragma once
+#include <iostream>
 #include<string>
 #include <vector>
 #include"Tablas.h"
 class Sector
 {
-	std::string datos;
-	std::vector<Tablas> tablas;
-	Sector(std::string dat, std::vector<Tablas> tab): datos(dat), tablas(tab) {}
+public:
+	std::vector<char> datos;
+	std::vector<Tablas> tabla;
+	Sector(size_t size, std::vector<Tablas> tabla);//acá asigno el tamaño del sector, supongo que sera un cin o en sql
+	bool insertar_datos(std::vector<char> data);//al principio queria meterle una tabla pero mejor lo pongo como tupla
+	void espacio_disp();
+	void set_datos();
 };
 
